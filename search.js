944c7d5e89e45b35f0af94577a4733cb907c30ec
Amalivz <116845283+Amalivz@users.noreply.github.com>
2023-04-08 18:06:16 -0400

    Search js
    not completed
    
    Co-Authored-By: vrinda2705 <126126499+vrinda2705@users.noreply.github.com>

---
+++ search.js
@@ -0,0 +1,47 @@
+function searchfunction(){
+    var searchstring = document.getElementById("searchp1").value;
+    var new1Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player1.html";
+    var new2Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player2.html";
+    var new3Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player3.html";
+    var new4Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player4.html";
+    var new5Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player5.html";
+    var new6Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player6.html";
+    var new7Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player7.html";
+    var new8Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player8.html";
+    var new9Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player9.html";
+    var new10Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player10.html";
+    var new11Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player11.html";
+    if(searchstring==="Marc-Andre Ter Stegen"){
+        window.location = new1Url;
+    }
+    else if(searchstring==="Ronald Araujo"){
+        window.location = new2Url;
+    }
+    else if(searchstring==="Marcos Alonso"){
+        window.location = new3Url;
+    }
+    else if(searchstring==="Jordi Alba"){
+        window.location = new4Url;
+    }
+    else if(searchstring==="Sergio Busquets"){
+        window.location = new5Url;
+    }
+    else if(searchstring==="Gavi"){
+        window.location = new6Url;
+    }
+    else if(searchstring==="Pedri"){
+        window.location = new7Url;
+    }
+    else if(searchstring==="Frenkie de Jong"){
+        window.location = new8Url;
+    }
+    else if(searchstring==="Ousmane Dembélé"){
+        window.location = new9Url;
+    }
+    else if(searchstring==="Robert Lewandowski"){
+        window.location = new10Url;
+    }
+    else if(searchstring==="Ansu Fati"){
+        window.location = new11Url;
+    }
+}
