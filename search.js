528a8b8bc9f73d704c9223f98c6dce7817c7d435
vrinda2705 <126126499+vrinda2705@users.noreply.github.com>
2023-04-08 20:48:52 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -1,47 +1,47 @@
 function searchfunction(){
     var searchstring = document.getElementById("searchp1").value;
-    var new1Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player1.html";
-    var new2Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player2.html";
-    var new3Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player3.html";
-    var new4Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player4.html";
-    var new5Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player5.html";
-    var new6Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player6.html";
-    var new7Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player7.html";
-    var new8Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player8.html";
-    var new9Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player9.html";
-    var new10Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player10.html";
-    var new11Url="file:///Users/jasveensinghsahani/Downloads/Encoder/player11.html";
+    var new1File="player1.html";
+    var new2File="player2.html";
+    var new3File="player3.html";
+    var new4File="player4.html";
+    var new5File="player5.html";
+    var new6File="player6.html";
+    var new7File="player7.html";
+    var new8File="player8.html";
+    var new9File="player9.html";
+    var new10File="player10.html";
+    var new11File="player11.html";
     if(searchstring==="Marc-Andre Ter Stegen"){
-        window.location = new1Url;
+        window.location = new1File;
     }
     else if(searchstring==="Ronald Araujo"){
-        window.location = new2Url;
+        window.location = new2File;
     }
     else if(searchstring==="Marcos Alonso"){
-        window.location = new3Url;
+        window.location = new3File;
     }
     else if(searchstring==="Jordi Alba"){
-        window.location = new4Url;
+        window.location = new4File;
     }
     else if(searchstring==="Sergio Busquets"){
-        window.location = new5Url;
+        window.location = new5File;
     }
     else if(searchstring==="Gavi"){
-        window.location = new6Url;
+        window.location = new6File;
     }
     else if(searchstring==="Pedri"){
-        window.location = new7Url;
+        window.location = new7File;
     }
     else if(searchstring==="Frenkie de Jong"){
-        window.location = new8Url;
+        window.location = new8File;
     }
     else if(searchstring==="Ousmane Dembélé"){
-        window.location = new9Url;
+        window.location = new9File;
     }
     else if(searchstring==="Robert Lewandowski"){
-        window.location = new10Url;
+        window.location = new10File;
     }
     else if(searchstring==="Ansu Fati"){
-        window.location = new11Url;
+        window.location = new11File;
     }
 }
